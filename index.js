0be0c0f1414e4c80086273ce083fd908f9df9486
Christian Samson <85580709+christianpsamson@users.noreply.github.com>
2023-10-20 14:36:32 -0400

    Wild Wild Life answered

--- index.js
+++ index.js
@@ -13,14 +13,31 @@
 //===================================================================================//
 //                                  Fizz Buzz
 //===================================================================================//
-for (i=0;i<100;i++){
-    if (i%3===0 && i%5===0){
-        console.log('FizzBuzz')
-    }else if(i%3===0){
-        console.log('Fizz');
-    }else if(i%5===0){
-        console.log('Buzz');
-    }else{
-        console.log(i);
-    }
-}
+// for (i=0;i<100;i++){
+//     if (i%3===0 && i%5===0){
+//         console.log('FizzBuzz')
+//     }else if(i%3===0){
+//         console.log('Fizz');
+//     }else if(i%5===0){
+//         console.log('Buzz');
+//     }else{
+//         console.log(i);
+//     }
+// }
+//===================================================================================//
+//                                 Wild Wild Life
+//===================================================================================//
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+const sharky = ["Sharky", "shark", 20, "Left Coast"];
+const plantee = ["Plantee", "plant", 5000, "Mordor"];
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
+const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"];
+plantee[2] = 50001;
+console.log(plantee);
+// Revisit this to explore other methods:
+wolfy[3] = "Gotham City";
+console.log(wolfy);
+dart[4] = "Hawkins";
+console.log(dart);
+wolfy[0] = "Gameboy";
+console.log(wolfy);
