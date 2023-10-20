97f95279d92d5d629ef6f9ea5d785ac411f07fd7
Christian Samson <85580709+christianpsamson@users.noreply.github.com>
2023-10-20 14:56:24 -0400

    Yell at the Ninja Turtles answered

--- index.js
+++ index.js
@@ -27,17 +27,24 @@
 //===================================================================================//
 //                                 Wild Wild Life
 //===================================================================================//
-const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
-const sharky = ["Sharky", "shark", 20, "Left Coast"];
-const plantee = ["Plantee", "plant", 5000, "Mordor"];
-const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
-const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"];
-plantee[2] = 50001;
-console.log(plantee);
-// Revisit this to explore other methods:
-wolfy[3] = "Gotham City";
-console.log(wolfy);
-dart[4] = "Hawkins";
-console.log(dart);
-wolfy[0] = "Gameboy";
-console.log(wolfy);
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+// const sharky = ["Sharky", "shark", 20, "Left Coast"];
+// const plantee = ["Plantee", "plant", 5000, "Mordor"];
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
+// const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"];
+// plantee[2] = 50001;
+// console.log(plantee);
+// // Revisit this to explore other methods:
+// wolfy[3] = "Gotham City";
+// console.log(wolfy);
+// dart[4] = "Hawkins";
+// console.log(dart);
+// wolfy[0] = "Gameboy";
+// console.log(wolfy);
+//===================================================================================//
+//                               Yell at the Ninja Turtles
+//===================================================================================//
+let ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"];
+for (let member of ninjaTurtles){
+    console.log(member.toUpperCase());
+}
